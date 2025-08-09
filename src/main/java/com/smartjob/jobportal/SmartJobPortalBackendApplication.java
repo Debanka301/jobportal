b29532f8a878857db971f8cf2c763e8ec438da99
Debanka301 <debankakhan926@gmail.com>
2025-08-09 12:23:50 +0530

    Initial commit - Code Skeleton

---
+++ src/main/java/com/smartjob/jobportal/SmartJobPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartjob.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJobPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartJobPortalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartjob/jobportal/SmartJobPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartjob.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartJobPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
